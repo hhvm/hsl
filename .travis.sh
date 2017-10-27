44466ffb8cb2dfdd07fc9faea1b25d7ff721342d
Fred Emmott <fe@fb.com>
2017-10-27 12:31:46 -0700

    Test with experimental shape options
    Summary:
    Also update dependencies to compatible versions
    Closes https://github.com/hhvm/hsl/pull/35
    
    Reviewed By: kmeht
    
    Differential Revision: D6174316
    
    Pulled By: kmeht
    
    fbshipit-source-id: 7016ed51446cd4be19d08858f6170cf9e9906e9a

--- .travis.sh
+++ .travis.sh
@@ -11,3 +11,9 @@ hhvm -d hhvm.php7.all=0 -d hhvm.jit=0 vendor/bin/phpunit
 hhvm -d hhvm.php7.all=1 -d hhvm.jit=0 vendor/bin/phpunit
 
 hhvm -d hhvm.jit=0 bin/generate-docs.php $(mktemp -d)
+
+HHVM_VERSION=$(hhvm --php -r 'echo HHVM_VERSION_ID;' 2>/dev/null);
+if [ $HHVM_VERSION -ge 32200 -a $HHVM_VERSION -lt 32300 ]; then
+  echo enable_experimental_tc_features = optional_shape_field, unknown_fields_shape_is_not_subtype_of_known_fields_shape >> .hhconfig
+  hh_server --check $(pwd)
+fi
