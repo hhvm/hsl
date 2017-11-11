c0c4bd1c939ddffcc6bdf821513e1909876c3e5a
Kunal Mehta <kunalm@fb.com>
2017-11-10 22:29:19 -0800

    Add integer constants to Math
    Reviewed By: periodic1236
    
    Differential Revision: D6306353
    
    fbshipit-source-id: 985781b30c09d37d94ae581b3c73f4319a0c295e

---
+++ src/math/constants.php
@@ -0,0 +1,16 @@
+<?hh // strict
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+namespace HH\Lib\Math;
+
+const int INT64_MAX = 9223372036854775807;
+const int INT64_MIN = namespace\INT64_MAX + 1; // -9223372036854775808
+const int INT53_MAX = 9007199254740992;
+const int INT53_MIN = -9007199254740993;
+const int INT32_MAX = 2147483647;
+const int INT32_MIN = -2147483648;
+const int INT16_MAX = 32767;
+const int INT16_MIN = -32768;
+
+const int UINT32_MAX = 4294967295;
+const int UINT16_MAX = 65535;
