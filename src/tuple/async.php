414937a288db32e982c21bc811fa82733ab04de5
Fred Emmott <fe@fb.com>
2017-11-22 14:43:03 -0800

    better documentation for Tuple\gen
    Reviewed By: kmeht
    
    Differential Revision: D6380828
    
    fbshipit-source-id: 761140053341f915bdc3eccdcef09e5ee4414cef

--- src/tuple/async.php
+++ src/tuple/async.php
@@ -12,9 +12,24 @@
 namespace HH\Lib\Tuple;
 
 /**
- * Given `(Awaitable<T1>, Awaitable<T2> ....)`, return `Awaitable<(T1, T2)>`.
+ * Create an awaitable tuple from variadic awaitables.
  *
- * `?Awaitable<T1>` is also supported.
+ * Given `(Awaitable<T1>, Awaitable<T2>, ...)`, returns
+ * `Awaitable(T1, T2, ...)`.
+ *
+ * Nullable Awaitables are also supported:
+ * `(?Awaitable<T1>, ?Awaitable<T2>, ...)` is transformed to
+ * `Awaitable<(?T1, ?T2)>`
+ *
+ * This is particularly useful when combined with list assignment:
+ *
+ * ```Hack
+ * list($a, $b, $c) = await Tuple\from_async(
+ *   foo_async(),
+ *   bar_async(),
+ *   baz_async(),
+ * );
+ * ```
  *
  * The function signature here is inaccurate as it can not be correctly
  * expressed in Hack; this function is special-cased in the typechecker.
