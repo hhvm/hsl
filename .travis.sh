796460931b3f7814431cce705e4332d8c851a86d
Fred Emmott <fe@fb.com>
2018-02-09 08:47:17 -0800

    OSS configuration updates
    - update .hhconfig to match what we require for the HSL
    - remove dev dependency on definition-finder; this was used for
    documentation generation, but we're now doing that on docs.hhvm.com
    instead
    - HHVM 3.23 is a hard dependency, not a dev dependency
    - update other dependencies
    - now we're packaging for Trusty again, we don't need to use docker on
    Travis; this makes test runs faster
    - remove stuff for docs updating from travis config

--- .travis.sh
+++ .travis.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 set -ex
 hhvm --version
-curl https://getcomposer.org/installer | hhvm -d hhvm.jit=0 --php -- /dev/stdin --install-dir=/usr/local/bin --filename=composer
 
-cd /var/source
-hhvm -d hhvm.jit=0 /usr/local/bin/composer install
+composer install
 
-hh_server --check $(pwd)
-hhvm -d hhvm.php7.all=0 -d hhvm.jit=0 vendor/bin/phpunit
-hhvm -d hhvm.php7.all=1 -d hhvm.jit=0 vendor/bin/phpunit
+hh_client
 
-sed -i '/enable_experimental_tc_features/d' .hhconfig
+hhvm vendor/bin/phpunit tests/
+
+echo > .hhconfig
 hh_server --check $(pwd)
