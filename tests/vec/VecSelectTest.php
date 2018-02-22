22931ba3f697f759b4d472c8d6ffe17761a6b3e2
Kunal Mehta <kunalm@fb.com>
2018-02-22 11:09:30 -0800

    HH_FIXMEs for argument unpacking arity errors
    Reviewed By: periodic1236, elize1979
    
    Differential Revision: D7053242
    
    fbshipit-source-id: a809be0852c3e26357d83cfa1126878ab8dddbcf

--- tests/vec/VecSelectTest.php
+++ tests/vec/VecSelectTest.php
@@ -44,6 +44,7 @@ final class VecSelectTest extends PHPUnit_Framework_TestCase {
     Container<Traversable<Tv>> $traversables,
     vec<Tv> $expected,
   ): void {
+    /* HH_FIXME[4104] Stricter enforcement of argument unpacking arity (T25385321) */
     expect(Vec\diff($base, ...$traversables))->toBeSame($expected);
   }
 
