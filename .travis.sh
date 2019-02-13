9872eddd12f54126b9c4ccebcb9f37c110a521a5
Fred Emmott <fe@fb.com>
2019-02-13 09:36:35 -0800

    Update open source test infra
    - use Docker on Travis
    - re-add composer.lock

--- .travis.sh
+++ .travis.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 set -ex
+apt update -y
+DEBIAN_FRONTEND=noninteractive apt install -y php-cli zip unzip
 hhvm --version
+php --version
+if [ ! -e .git/refs/heads/master ]; then
+  # - Travis clones with `--branch`, then moves to a detached HEAD state
+  # - if we're on a detached HEAD, Composer uses master to resolve branch
+  #   aliases.
+  # So, create the master branch :p
+  git branch master HEAD
+fi
 
+(
+  cd $(mktemp -d)
+  curl https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+)
 composer install
 
 hh_client
-
-vendor/bin/hacktest tests/
-
-echo > .hhconfig
-hh_server --check $(pwd)
+hhvm vendor/bin/hacktest tests/
