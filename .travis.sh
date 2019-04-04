9c70555b4573aebf1578006dd3c8675802678fdf
Fred Emmott <fe@fb.com>
2019-04-04 13:38:26 -0700

    Allow test failures on release (#80)
    Summary:
    We test against nightlies for master
    Pull Request resolved: https://github.com/hhvm/hsl/pull/80
    
    Differential Revision: D14779391
    
    fbshipit-source-id: f5664a7471eacc79dd2699a960f4dcd9bf4edaee

--- .travis.sh
+++ .travis.sh
@@ -16,7 +16,12 @@ fi
   cd $(mktemp -d)
   curl https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 )
+if (hhvm --version | grep -q -- -dev); then
+  # Doesn't exist in master, but keep it here so that we can test release
+  # branches on nightlies too
+  rm -f composer.lock
+fi
 composer install
 
 hh_client
-hhvm vendor/bin/hacktest tests/
+vendor/bin/hacktest tests/
