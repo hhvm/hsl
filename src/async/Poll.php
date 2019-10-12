99f223649efae32bdf028271ac95efb0ffffaa65
Fred Emmott <fe@fb.com>
2019-10-11 21:01:45 -0700

    add doccomments to Async\*Poll and Async\Semaphore
    Summary: Let's put this on the docs site when released.
    
    Reviewed By: jjergus
    
    Differential Revision: D17882456
    
    fbshipit-source-id: a96a258eed50c11b58de2aaf04631924904f53d8

--- src/async/Poll.php
+++ src/async/Poll.php
@@ -10,7 +10,8 @@
 
 namespace HH\Lib\Async;
 
-/**
+/** An async poll/select equivalent for traversables without a related key.
+ *
  * ===== WARNING ===== WARNING ===== WARNING ===== WARNING ===== WARNING =====
  *
  * See detailed warning at top of `BasePoll`
@@ -19,15 +20,24 @@ namespace HH\Lib\Async;
  */
 
 final class Poll<Tv> extends BasePoll<mixed, Tv> implements AsyncIterator<Tv> {
-
+  /** Create a Poll from the specified list of awaitables.
+   *
+   * See `KeyedPoll` if you have a `KeyedTraversable` and want to preserve
+   * keys.
+   */
   public static function from(Traversable<Awaitable<Tv>> $awaitables): this {
     return self::fromImpl(new Vector($awaitables));
   }
 
+  /** Add an additional awaitable to the poll. */
   public function add(Awaitable<Tv> $awaitable): void {
     $this->addImpl(null, $awaitable);
   }
 
+  /** Add multiple additional awaitables to the poll.
+   *
+   * See `KeyedPoll` if you have a `KeyedTraversable` and want to preserve keys.
+   */
   public function addMulti(Traversable<Awaitable<Tv>> $awaitables): void {
     $this->addMultiImpl(new Vector($awaitables));
   }
