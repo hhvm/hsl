a1d3092357df1c8c52f1233be45009a44e647b32
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2020-01-25 13:40:41 +0100

    Revert define PHP 7.1

--- .travis.sh
+++ .travis.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 apt update -y
-DEBIAN_FRONTEND=noninteractive apt install -y php7.1-cli zip unzip
+DEBIAN_FRONTEND=noninteractive apt install -y php-cli zip unzip
 hhvm --version
 php --version
 if [ ! -e .git/refs/heads/master ]; then
