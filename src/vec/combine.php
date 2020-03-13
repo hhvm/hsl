f3ce30ca1348b26df688dd64d23c8572479187ae
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2020-03-13 13:50:55 -0700

    Add Vec\zip() to the HSL (#127)
    Summary:
    I believe this already exists as `Vec\fb\zip()`.
    If the fb impl is different / better somehow, it could also be promoted out of the fb namespace.
    
    I've been using `Vec\Lex\zip()` for months and it is a very useful tool in my toolbox.
    I use it in case where I need to iterate over two things at once, but don't want to manipulate raw indexes using a `for($i = 0;...)` loop.
    
    ```HACK
    $vec1 = vec[...];
    $vec2 = vec[...];
    foreach(Vec\Lex\zip($vec1, $vec2) as list($one, $two)){
      // code...
    }
    ```
    
    I am making this PR to get the ball rolling on other useful HSL functions.
    F.e. `C\fb\pop_back()` and `Vec\gen_map_limited_concurrency()`.
    Pull Request resolved: https://github.com/hhvm/hsl/pull/127
    
    Reviewed By: jjergus
    
    Differential Revision: D20349461
    
    Pulled By: fredemmott
    
    fbshipit-source-id: 99f6287e692a7ee1a822b4cc81036935e01c4428

--- src/vec/combine.php
+++ src/vec/combine.php
@@ -10,6 +10,8 @@
 
 namespace HH\Lib\Vec;
 
+use namespace HH\Lib\{C, Math};
+
 /**
  * Returns a new vec formed by concatenating the given Traversables together.
  *
@@ -22,8 +24,7 @@ namespace HH\Lib\Vec;
  */
 <<__Rx, __AtMostRxAsArgs>>
 function concat<Tv>(
-  <<__OnlyRxIfImpl(\HH\Rx\Traversable::class)>>
-  Traversable<Tv> $first,
+  <<__OnlyRxIfImpl(\HH\Rx\Traversable::class)>> Traversable<Tv> $first,
   Container<Tv> ...$rest
 ): vec<Tv> {
   $result = vec($first);
@@ -31,6 +32,37 @@ function concat<Tv>(
     foreach ($traversable as $value) {
       $result[] = $value;
     }
+  }
+  return $result;
+}
+
+/**
+ * Returns a vec where each element is a tuple (pair) that combines, pairwise,
+ * the elements of the two given Traversables.
+ *
+ * If the Traversables are not of equal length, the result will have
+ * the same number of elements as the shortest Traversable.
+ * Elements of the longer Traversable after the length of the shorter one
+ * will be ignored.
+ *
+ * Time complexity: O(min(m, n)), where m is the size of `$first` and n is the
+ * size of `$second`
+ * Space complexity: O(min(m, n)), where m is the size of `$first` and n is the
+ * size of `$second`
+ */
+<<__Rx, __AtMostRxAsArgs>>
+function zip<Tv, Tu>(
+  <<__MaybeMutable, __OnlyRxIfImpl(\HH\Rx\Traversable::class)>>
+  Traversable<Tv> $first,
+  <<__MaybeMutable, __OnlyRxIfImpl(\HH\Rx\Traversable::class)>>
+  Traversable<Tu> $second,
+): vec<(Tv, Tu)> {
+  $one = vec($first);
+  $two = vec($second);
+  $result = vec[];
+  $lesser_count = Math\minva(C\count($one), C\count($two));
+  for ($i = 0; $i < $lesser_count; ++$i) {
+    $result[] = tuple($one[$i], $two[$i]);
   }
   return $result;
 }
--- tests/vec/VecCombineTest.php
+++ tests/vec/VecCombineTest.php
@@ -17,15 +17,14 @@ final class VecCombineTest extends HackTest {
 
   public static function provideTestConcat(): varray<mixed> {
     return varray[
-      tuple(
-        varray[],
-        varray[],
-        vec[],
-      ),
+      tuple(varray[], varray[], vec[]),
       tuple(
         vec[],
         varray[
-          darray[], Vector {}, Map {}, Set {},
+          darray[],
+          Vector {},
+          Map {},
+          Set {},
         ],
         vec[],
       ),
@@ -48,6 +47,81 @@ final class VecCombineTest extends HackTest {
     vec<Tv> $expected,
   ): void {
     expect(Vec\concat($first, ...$rest))->toEqual($expected);
+  }
+
+  public static function provideTestZip(
+  ): dict<
+    string,
+    (Traversable<mixed>, Traversable<mixed>, Traversable<(mixed, mixed)>),
+  > {
+    return dict[
+      "Should return empty by zipping 2 empty vecs" =>
+        tuple(vec[], vec[], vec[]),
+      "Should return empty if any of the vecs is empty" =>
+        tuple(vec[], vec[1, 2, 3], vec[]),
+      "Should zip all elements if vecs are of the same size" => tuple(
+        vec[1, 2, 3],
+        vec[4, 5, 6],
+        vec[tuple(1, 4), tuple(2, 5), tuple(3, 6)],
+      ),
+      "We can zip vecs of different types" => tuple(
+        vec[1, 2, 3],
+        vec['a', 'b', 'c'],
+        vec[tuple(1, 'a'), tuple(2, 'b'), tuple(3, 'c')],
+      ),
+      "The inputs can be any traversables, but keys are discarded." => tuple(
+        darray['a' => 1, 'b' => 2, 'c' => 3],
+        Map {10 => 'd', 20 => 'e', 30 => 'f'},
+        vec[tuple(1, 'd'), tuple(2, 'e'), tuple(3, 'f')],
+      ),
+      'empty dict and empty vec' => tuple(dict[], vec[], vec[]),
+      'one element' => tuple(vec['a'], dict[0 => 'b'], vec[tuple('a', 'b')]),
+      'first is longer than second' =>
+        tuple(vec[1, -1], vec[2], vec[tuple(1, 2)]),
+      'second is longer than first' =>
+        tuple(vec['yes'], vec['maybe', 'no'], vec[tuple('yes', 'maybe')]),
+      'very long input' => tuple(
+        Vec\range(0, 101),
+        Vec\range(0, 101),
+        Vec\map(Vec\range(0, 101), (int $i) ==> tuple($i, $i)),
+      ),
+      'Hack Collections' => tuple(
+        Map {'banana' => 5, 'pear' => 4},
+        Set {3, 2},
+        vec[tuple(5, 3), tuple(4, 2)],
+      ),
+      'Generators' => tuple(
+        HackLibTestTraversables::getKeyedIterator(dict[
+          'keys' => 'do',
+          'not' => 'matter',
+          'for' => 'vec',
+          'zip' => 'anyhow',
+        ]),
+        HackLibTestTraversables::getIterator(vec[
+          'generators',
+          'are',
+          'very',
+          'powerful',
+          'indeed',
+        ]),
+        vec[
+          tuple('do', 'generators'),
+          tuple('matter', 'are'),
+          tuple('vec', 'very'),
+          tuple('anyhow', 'powerful'),
+        ],
+      ),
+    ];
+  }
+
+  <<DataProvider('provideTestZip')>>
+  public function testZip(
+    Traversable<mixed> $first,
+    Traversable<mixed> $second,
+    vec<(mixed, mixed)> $expected,
+  ): void {
+    $actual = Vec\zip($first, $second);
+    expect($actual)->toEqual($expected);
   }
 
 }
