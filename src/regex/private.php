36e97917495c33baca3248be9caf9beaf83af170
Ján <jjergus@fb.com>
2020-04-27 16:38:09 -0700

    FIXME still required for open-source (#133)
    Summary:
    - const was added to a FB-only hhi file
    - even if added to an open-source hhi file, we'd like to wait a few weeks before removing the FIXME, so that HSL can support multiple recent versions of HHVM
    
    Pull Request resolved: https://github.com/hhvm/hsl/pull/133
    
    Test Plan: `hh_client` in a local checkout of https://github.com/hhvm/hsl/
    
    Reviewed By: alexeyt, vassilmladenov
    
    Differential Revision: D21260180
    
    Pulled By: jjergus
    
    fbshipit-source-id: 698406deec6868fc3f235c9d243e33e012479e44

--- src/regex/private.php
+++ src/regex/private.php
@@ -45,6 +45,8 @@ function regex_match<T as Regex\Match>(
     /* HH_IGNORE_ERROR[4110] */ $pattern,
     $haystack,
     inout $match,
+    /* HH_IGNORE_ERROR[2049] using FB__PRIVATE const in open-source code */
+    /* HH_IGNORE_ERROR[4106] using FB__PRIVATE const in open-source code */
     \PREG_FB__PRIVATE__HSL_IMPL | \PREG_OFFSET_CAPTURE,
     $offset,
   );
