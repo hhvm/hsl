55c5eb73f031f3146eeae08a9318884588f3ab1b
Adam Kramer <akramer@fb.com>
2020-05-12 17:27:25 -0700

    EntEvent: getName --> genName fix
    Summary: This is annoying me in slog so I fixed it.
    
    Reviewed By: alexeyt
    
    Differential Revision: D21528176
    
    fbshipit-source-id: 9c9caa44a33614a2d3b499fc509fa6bf751a50dc

--- src/vec/order.php
+++ src/vec/order.php
@@ -16,7 +16,8 @@ use namespace HH\Lib\{C, Dict, Math, Str};
  * Returns a new vec containing the range of numbers from `$start` to `$end`
  * inclusive, with the step between elements being `$step` if provided, or 1 by
  * default. If `$start > $end`, it returns a descending range instead of
- * an empty one.
+ * an empty one. If you are searching for Vec\seq or Vec\sequence, this is
+ * what you want.
  *
  * Time complexity: O(n), where `n` is the size of the resulting vec
  * Space complexity: O(n), where `n` is the size of the resulting vec
